97b95366c6b20e8c0bfaaba6bf69c3ac8dc48a44
Lugon LQ <48004924+lugondev@users.noreply.github.com>
2022-10-24 18:54:54 +0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles.css";
+
+import Test from "./Test";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Test />
+    </div>
+  );
+}
